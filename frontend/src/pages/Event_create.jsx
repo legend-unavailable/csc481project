a1e5251b9b8d8c60fb056a5d01578cc16ef0ae80
pc <joseaguilar11152@gmail.com>
2024-11-13 20:55:10 -0800

    working on event creation

--- frontend/src/pages/Event_create.jsx
+++ frontend/src/pages/Event_create.jsx
@@ -1,35 +1,83 @@
 import { useState } from "react";
 
 const Event_create = () => {
-    const [showOnl, setShowOnl] = useState(false);
-    const [showInp, setShowInp] = useState(false);
+    const [showInvites, setShowInvites] = useState(false);
+    const [showList, setShowList] = useState(false);
+    const [showForm, setShowForm] = useState(true);
+
     const dblSpace = <><br /><br /></>;
-    return(
-        <>
+
+    const addGuests = (event) => {
+        event.preventDefault();
+        
+
+    }
+    const guests = [{name: 'bob', email: 'bob@wahoo.com'}];
+    const events = [];
+    const list = guests.map(guest => {
+        let id = 0
+        return(
+            <li key={id++}>Name: {guest.name} | Email: {guest.email} <br /></li>
+        )
+    });
+    const hide = (event) => {
+        setShowForm(false);
+        setShowInvites(true);
+        event.
+    }
+    return (
+      <>
         <form>
-            <label htmlFor="name">Enter a name for your event?</label><br />
-            <input type="text" />{dblSpace}
+          {showForm && (
+            <form onSubmit={hide}>
+              <label htmlFor="name">Enter a name for your event?</label>
+              <br />
+              <input type="text" required />
+              {dblSpace}
 
-            <label htmlFor="type">Specify what type of event you want to create?</label>
-            <div id='eventType'>
-                <input type="radio" name="opt1" id="" /> Online
-                <input type="radio" name="opt2" id="" />In Person
-            </div><br />
+              <label htmlFor="type">
+                Specify what type of event you want to create?
+              </label>
+              <div id="eventType">
+                <input type="radio" name="opt1" id="" required /> Online
+                <input type="radio" name="opt2" id="" />
+                In Person
+              </div>
+              <br />
 
-            <label htmlFor="where">Where is the event taking place?</label><br />
-            {showOnl && <><input type="text" placeholder="e.g. Zoom"/>{dblSpace}</>}
-            {showInp && <><input type="text" placeholder="e.g. Zoom"/>{dblSpace}</>}
-            
+              <label htmlFor="where">Where is the event taking place?</label>
+              <br />
+              <input type="text" placeholder="e.g. Zoom" required />
+              {dblSpace}
 
-            <label htmlFor="when">When is the event taking place?</label><br />
-            <input type="datetime-local" name="starTime" id="" />{dblSpace}
+              <label htmlFor="when">When is the event taking place?</label>
+              <br />
+              <input type="datetime-local" name="starTime" required />
+              {dblSpace}
 
-            <label htmlFor="length">How long is the event?</label><br />
-            <input type="text" placeholder="e.g. 3 hours"/>{dblSpace}
+              <label htmlFor="length">How long is the event?</label>
+              <br />
+              <input type="text" placeholder="e.g. 3 hours" required />
+              {dblSpace}
 
+              <button type="submit">Create Event</button>
+            </form>
+          )}
 
+          {showInvites && (
+            <form onSubmit={addGuests}>
+              <label htmlFor="people">
+                Enter the names and emails of all the people who are invited
+              </label>
+              <br />
+              <input type="text" name="names" required />
+              <input type="email" name="guests" required />
+              <button>Add</button>
+              {showList && list}
+            </form>
+          )}
         </form>
-        </>
+      </>
     );
 }
 export default Event_create
